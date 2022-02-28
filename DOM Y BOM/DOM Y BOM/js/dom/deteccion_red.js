1aa1dd0cd7c6ed2ec3cdedc92de282c68ed60f62
jbajanam4 <jbajanam4@unemi.edu.ec>
2022-02-27 20:52:25 -0500

    primer proyecto

---
+++ DOM Y BOM/DOM Y BOM/js/dom/deteccion_red.js
@@ -0,0 +1,23 @@
+const d= document,
+w= window,
+n= navigator;
+
+export default function networkStatus(){
+const isOnline=()=>{
+const $div = d.createElement("div");
+if(n.onLine){
+    $div.textContent="Conexion Restablecida";
+    $div.classList.add("online");
+    $div.classList.remove("offine");
+    
+}else{
+    $div.textContent= "Conexion perdida";
+    $div.classList.add("offine");
+    $div.classList.remove("online");
+}
+d.body.insertAdjacentElement("afterbegin",$div);
+setTimeout(()=>d.body.removeChild($div),2000);
+};
+w.addEventListener("online",(e)=> isOnline());
+w.addEventListener("offine",(e)=> isOnline());
+}
